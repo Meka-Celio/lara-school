ee0bd9f67707cddc4fda4115e40f35141ce019a3
Meka-Celio <nnhaud.celestin@gmail.com>
2024-10-25 09:47:47 +0100

    Add Livewire and Jetstream

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,22 @@
 <?php
 
+use App\Http\Controllers\NiveauController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+
+    Route::prefix('niveaux')->group(function () {
+        Route::get('/', [NiveauController::class, 'index']);
+    });
 });
